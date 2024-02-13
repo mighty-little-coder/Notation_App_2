e8334779ca6a5a39f41390c785933b02fcdc0cc4
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2024-02-13 01:48:04 -0500

    Added to serverr.js and apiRoutes

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
+const path = require('path');
 const htmlRoutes = require('./htmlRoutes')
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+}); 
 
 router.use(htmlRoutes);
 
